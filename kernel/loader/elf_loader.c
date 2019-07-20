c7d3f394354db9c04bb846c0d4fb7260f07d1158
err0x5dd <err0x5dd@tkleinert.de>
2019-07-20 17:25:41 +0200

    Started working on an ELF file loader

--- kernel/loader/elf_loader.c
+++ kernel/loader/elf_loader.c
@@ -4,36 +4,120 @@
 #include "../include/multitasking.h"
 #include "../include/mm.h"
 
-void init_elf(void* image) {
-    struct elf_header* header = image;
-    struct elf_program_header* ph;
-    int i;
+// Uncomment for debug info
+#define DEBUG
 
-    if(header->magic != ELF_MAGIC) {
-        kprintf("ELF-Magic not valid!\n");
-        return;
+void init_elf(void* file) {
+    Elf32_Ehdr* ehdr = file;
+
+    if(check_elf(ehdr) != 0) {
+	#ifdef DEBUG
+	kprintf("[DEBUG] [loader] [elf] Not a valid ELF file\n");
+	#endif
+
+	return;
     }
 
-    ph = (struct elf_program_header*) (((char*) image) + header->ph_offset);
-    for(i = 0; i < header->ph_entry_count; i++, ph++) {
-        void* dest = (void*) ph->virt_addr;
-        void* src = ((char*) image) + ph->offset;
-        
-        if(ph->type != 1) {
-            continue;
-        }
+    if(ehdr->e_machine != EM_386) {
+	#ifdef DEBUG
+	kprintf("[DEBUG] [loader] [elf] ELF file is not for i386\n");
+	#endif
 
-	kprintf("[DEBUG] [elf] Alloc pages for module\n");
-
-	for(uint32_t j = 0; j < dest + ph->file_size; j += PAGE_SIZE) {
-	    uintptr_t memreq = pmm_alloc();
-	    vmm_map_page(NULL, dest + j, memreq);
-	}
-
-        memset(dest, 0, ph->mem_size);
-        memcpy(dest, src, ph->file_size);
+	return;
     }
 
-    kprintf("Start new task at %x\n", header->entry);
-    init_task((void*) header->entry);
+    if(ehdr->e_type != ET_EXEC) {
+	#ifdef DEBUG
+	kprintf("[DEBUG] [loader] [elf] Not an executable file\n");
+	#endif
+
+	return;
+    }
+
+    #ifdef DEBUG
+    kprintf("\n");
+    
+    kprintf("[DEBUG] [loader] [elf] e_ident:     ");
+    for(uint8_t i = 0; i < EI_PAD; i++) {
+	kprintf("%x ", ehdr->e_ident[i]);
+    }
+    kprintf("\n");
+    kprintf("[DEBUG] [loader] [elf] e_type:      %x\n", ehdr->e_type);
+    kprintf("[DEBUG] [loader] [elf] e_machine:   %x\n", ehdr->e_machine);
+    kprintf("[DEBUG] [loader] [elf] e_version:   %x\n", ehdr->e_version);
+    kprintf("[DEBUG] [loader] [elf] e_entry:     %x\n", ehdr->e_entry);
+    kprintf("[DEBUG] [loader] [elf] e_phoff:     %x\n", ehdr->e_phoff);
+    kprintf("[DEBUG] [loader] [elf] e_shoff:     %x\n", ehdr->e_shoff);
+    kprintf("[DEBUG] [loader] [elf] e_flags:     %x\n", ehdr->e_flags);
+    kprintf("[DEBUG] [loader] [elf] e_ehsize:    %x\n", ehdr->e_ehsize);
+    kprintf("[DEBUG] [loader] [elf] e_phentsize: %x\n", ehdr->e_phentsize);
+    kprintf("[DEBUG] [loader] [elf] e_phnum:     %x\n", ehdr->e_phnum);
+    kprintf("[DEBUG] [loader] [elf] e_shentsize: %x\n", ehdr->e_shentsize);
+    kprintf("[DEBUG] [loader] [elf] e_shnum:     %x\n", ehdr->e_shnum);
+    kprintf("[DEBUG] [loader] [elf] e_shstrndx:  %x\n", ehdr->e_shstrndx);
+    kprintf("\n");
+    kprintf("Loop\n");
+    while(1);
+    #endif
+
+    //kprintf("[loader] [elf] Start new task at %x\n", file);
+    init_task((void*) file);
 }
+
+int check_elf(Elf32_Ehdr* ehdr) {
+    // Check for ELF magic
+    if(ehdr->e_ident[EI_MAG0] == ELFMAG0 && ehdr->e_ident[EI_MAG1] == ELFMAG1 && ehdr->e_ident[EI_MAG2] == ELFMAG2 && ehdr->e_ident[EI_MAG3] == ELFMAG3) {
+	#ifdef DEBUG
+	kprintf("[DEBUG] [loader] [elf] Valid ELF magic\n");
+	#endif
+
+	// Check for 32-bit file
+	if(ehdr->e_ident[EI_CLASS] == ELFCLASS32) {
+	    #ifdef DEBUG
+	    kprintf("[DEBUG] [loader] [elf] Valid 32-bit file\n");
+	    #endif
+
+	    // Check for LSB encoding
+	    if(ehdr->e_ident[EI_DATA] == ELFDATA2LSB) {
+		#ifdef DEBUG
+		kprintf("[DEBUG] [loader] [elf] Valid LSB encoding\n");
+		#endif
+
+		// Check for ELF version
+		if(ehdr->e_ident[EI_VERSION] == EV_CURRENT && ehdr->e_version == EV_CURRENT) {
+		    #ifdef DEBUG
+		    kprintf("[DEBUG] [loader] [elf] Valid ELF version\n");
+		    #endif
+
+		    return 0;
+		} else {
+		    #ifdef DEBUG
+		    kprintf("[DEBUG] [loader] [elf] Not a valid ELF version\n");
+		    #endif
+
+		    return 4;
+		}
+	    } else {
+		#ifdef DEBUG
+		kprintf("[DEBUG] [loader] [elf] Not a valid LSB encoding\n");
+		#endif
+
+		return 3;
+	    }
+	} else {
+	    #ifdef DEBUG
+	    kprintf("[DEBUG] [loader] [elf] Not a 32-bit file\n");
+	    #endif
+
+	    return 2;
+	}
+    } else {
+	#ifdef DEBUG
+	kprintf("[DEBUG] [loader] [elf] Not a valid ELF magic\n");
+	#endif
+
+	return 1;
+    }
+}
+
+
